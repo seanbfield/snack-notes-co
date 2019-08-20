4663e88f769f362ff7a21ebef989133912da5b49
seanbfield <rhblanchfield@programmer.net>
2019-08-20 13:41:05 -0400

    auth functioning

---
+++ client/src/components/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Landing() {
+  return (
+    <div className="landing-view">
+      <div className="App-header">
+        <h1>Home</h1>
+      </div>
+    </div>
+
+  )
+}
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_101746) do
+ActiveRecord::Schema.define(version: 2019_08_20_155305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2019_08_20_101746) do
   create_table "ideas", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
